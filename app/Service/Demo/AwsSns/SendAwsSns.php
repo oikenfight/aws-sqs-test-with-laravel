735c82061685f175b4e1c60658994511c72d6690
YutaOikawa <oikenfight@gmail.com>
2018-09-19 16:59:18 +0900

    create AwsSns Service for demo.

---
+++ app/Service/Demo/AwsSns/SendAwsSns.php
@@ -0,0 +1,41 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Service\Demo\AwsSns;
+
+use Aws\AwsClient;
+use Aws\Exception\AwsException;
+use Aws\Sns\SnsClient;
+use Illuminate\Support\Facades\App;
+use AWS;
+
+/**
+ * Class AwsSnsSend
+ * @package App\Service\Demo\AwsSns
+ */
+class SendAwsSns extends AwsSns
+{
+    /**
+     * @param $message
+     * @return \Aws\Result
+     */
+    public function __invoke($phoneNumber, $message)
+    {
+        \Log::debug($phoneNumber);
+        \Log::debug($message);
+
+        $params = [
+            // 'MessageAttributes' => [],
+            // 'MessageBody' => $message,
+            'Message' => $message,
+            'PhoneNumber' => $phoneNumber,
+        ];
+
+        $result = $this->client->publish($params);
+
+        \Log::debug('sent by sns');
+        \Log::debug($result);
+
+        return $result;
+    }
+}
