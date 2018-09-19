944d093e9e40c1e6c4523f2a939095f23ad6f259
YutaOikawa <oikenfight@gmail.com>
2018-09-19 15:45:02 +0900

    create top brade.

---
+++ resources/views/demo/index.blade.php
@@ -0,0 +1,22 @@
+{{-- layout --}}
+@extends('layouts.default')
+
+{{-- Additional Css --}}
+@section('additionalCss')
+@endsection
+
+{{-- Additional JavaScript --}}
+@section('additionalJs')
+@endsection
+
+{{-- titleSuffix --}}
+@section('titleSuffix', 'demo')
+
+{{-- Content --}}
+@section('content')
+    <div>
+        <div id="demo-app">
+            <demo-app-component></demo-app-component>
+        </div>
+    </div>
+@endsection
