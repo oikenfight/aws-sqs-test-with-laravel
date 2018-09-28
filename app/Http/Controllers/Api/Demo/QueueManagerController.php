40ac341564b5c17dff68ea501ec5f359867e1b18
YutaOikawa <oikenfight@gmail.com>
2018-09-28 15:54:54 +0900

    reimplement QueueManagerController to use redis.

--- app/Http/Controllers/Api/Demo/QueueManagerController.php
+++ app/Http/Controllers/Api/Demo/QueueManagerController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers\Api\Demo;
 
 use App\Service\Demo\AwsSqs\DeleteAwsSqs;
 use App\Service\Demo\AwsSqs\GetAwsSqs;
+use App\Service\Demo\Redis\SetQueues;
 use Aws\Exception\AwsException;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -15,7 +17,12 @@ use App\Http\Controllers\Controller;
  */
 final class QueueManagerController extends Controller
 {
-    public function get(GetAwsSqs $getAwsSws)
+    /**
+     * @param GetAwsSqs $getAwsSws
+     * @param SetQueues $setQueues
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function get(GetAwsSqs $getAwsSws, SetQueues $setQueues)
     {
         try{
             $data = $getAwsSws();
@@ -28,30 +35,28 @@ final class QueueManagerController extends Controller
         $queues = [];
         if($data){
             foreach($data as $item){
-                $queues[] = $item;
+                $body = json_decode($item['Body']);
+                $message = json_decode($body->Message);
+                $value = [
+                    'device' => $message->device,
+                    'body' => $message->body,
+                    'ReceiptHandle' => $item['ReceiptHandle'],
+                    'received_at' => Carbon::now(),
+                ];
+                $queues[] = $value;
             }
         }
 
-        // \Log::debug('~~~~ get ~~~~~~~~~~~~~~~~~~~~~~~~~');
-        // foreach ($queues as $message) {
-        //     \Log::debug('-----------------');
-        //     \Log::debug("message:" . $message['Body']);
-        //     \Log::debug("ReceiptHandle:" . $message['ReceiptHandle']);
-        // }
-        // \Log::debug('');
+        $setQueues($queues);
 
         return response()->json([
             'result' => 'ok',
-            'queues' => $queues,
+            // 'queues' => $queues,
         ], 200);
     }
 
     public function destroy(Request $request, DeleteAwsSqs $deleteAwsSqs)
     {
-        // TODO: 現状だと ReceiptHandle のみから削除してるが、これは更新されている可能性がある。
-        // device（電話番号）から QueueManager で管理してる最新の ReceiptHandle を検索して、それを用いるべき。
-        // redis 辺りを使うとできるようになりそう。
-
         $data = $request->all();
 
         \Log::debug('=== delete ================');
