deabff6f4e5cc4e180e36637d13c33ef10374f09
YutaOikawa <oikenfight@gmail.com>
2018-09-28 15:58:09 +0900

    reimplement QueueManagerAppComponent.vue with using redis api.

--- resources/js/components/Demo/QueueManagerAppComponent.vue
+++ resources/js/components/Demo/QueueManagerAppComponent.vue
@@ -19,36 +19,17 @@
                     </div>
                     <!-- content -->
                     <div class="row">
-                        <!-- Response Queues -->
-                        <div class="col-6">
+                        <!-- All Queues in Redis -->
+                        <div class="col-12">
                             <div class="card">
                                 <div class="card-header">
-                                    Queues got from sqs
+                                    All Queues QueueManager has.
                                 </div>
                                 <div class="card-body">
                                     <ul class="list-group">
-                                        <li class="list-group-item" v-for="queue in responseQueues">
-                                            <div class="float-left">
-                                                <p class="card-text">
-                                                    device: {{ queue.Body.Message.device }} <br/>
-                                                    body: {{ queue.Body.Message.body }}
-                                                </p>
-                                            </div>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                        <!-- Controlling Queues -->
-                        <div class="col-6">
-                            <div class="card">
-                                <div class="card-header">
-                                    Queues Backend Apps are controlling
-                                </div>
-                                <div class="card-body">
-                                    <ul class="list-group">
-                                        <li class="list-group-item" v-for="queue in controlledQueues">
+                                        <li class="list-group-item" v-for="queue in allQueues">
                                             <div class="float-right">
+                                                <!-- TODO: add expired_at -->
                                                 expired: <span class="badge badge-light">{{ queue.expired_time }}</span>
                                             </div>
                                             <div class="float-left">
@@ -73,88 +54,32 @@
     export default {
         data () {
             return {
-                responseQueues: [],
-                controlledQueues: [],
+                allQueues: [],
                 running: false,
-                devices: {
-                    backendA: [
-                        '08000000000',
-                        '08000000001',
-                        '08000000002',
-                        '08000000003',
-                    ],
-                    backendB: [
-                        '08000000004',
-                        '08000000005',
-                        '08000000006',
-                    ],
-                    backendC: [
-                        '08000000007',
-                        '08000000008',
-                        '08000000009',
-                    ],
-                }
             }
-        },
-        mounted () {
-            setInterval(() => {
-                // 期限切れ Queue を削除
-                Object.keys(this.controlledQueues).forEach((val, index) => {
-                    if (this.controlledQueues[index].expired_time >= 0) {
-                        // オリジナルのこれが更新されれば、index.state.backendQueues の queue も更新される。
-                        this.controlledQueues[index].expired_time -= 1
-                    } else {
-                        this.controlledQueues.splice(index, 1)
-                    }
-                })
-            }, 1000)
         },
         methods: {
             startBatch () {
                 this.running = true
+                // Queue Manager の継続的な処理をスタート
                 let batch = setInterval(() => {
+
+                    // aws sqs から queue を取得し、QueueManagerService で可動してる Redis に保存してる
                     axios.get('api/demo/queue_manager/get')
                     .then(response => {
                         // parse response to json
-                        this.responseQueues = response.data.queues.map((val) => {
-                            val.Body = JSON.parse(val.Body)
-                            val.Body.Message = JSON.parse(val.Body.Message)
-                            return val
-                        })
+                        console.log('queue manager get from aws sqs.')
+                        console.log(response.data)
+                    });
 
-                        console.log(this.responseQueues)
-
-                        // 受け取ったデータを追加
-                        this.responseQueues.forEach((val, index) => {
-                            let targetDevice = val.Body.Message.device
-                            let key = this.controlledQueues.findIndex((queue) => queue.device === targetDevice)
-                            if (key === -1) {
-                                let queue = {
-                                    'device': targetDevice,
-                                    'body': val.Body.Message.body,
-                                    'ReceiptHandle': val.ReceiptHandle,
-                                    'expired_time': 25,    // 処理中を想定して、データを保持する時間
-                                }
-
-                                // 受け取った Queue が処理中でなかった場合
-                                this.controlledQueues.push(queue)
-
-                                // targetDevice（電話番号）から、適切な BackendApp にリクエスト
-                                let targetBackendApp = ''
-                                if (this.devices.backendA.indexOf(queue.device) > -1) {
-                                    targetBackendApp = 'backendA'
-                                } else if (this.devices.backendB.indexOf(queue.device) > -1) {
-                                    targetBackendApp = 'backendB'
-                                } else if (this.devices.backendC.indexOf(queue.device) > -1) {
-                                    targetBackendApp = 'backendC'
-                                }
-                                console.log(queue)
-                                console.log(queue.device)
-                                console.log('targetApp: ' + targetBackendApp)
-                                this.sendToBackendApp(targetBackendApp, queue)
-                            }
-                        })
-                    })
+                    // Web 上に可視化するために Redis からデータを取得してる。
+                    axios.get('api/demo/redis/queues')
+                    .then(response => {
+                        // parse response to json
+                        console.log('queue manager get all queues from redis database.')
+                        console.log(response.data.queues)
+                        this.allQueues = response.data.queues
+                    });
 
                     // ストップボタンが押されたら Interval 停止
                     if (!this.running) {
@@ -163,15 +88,8 @@
                 }, 3000)
             },
             stopBatch () {
+                // Queue Manager の継続的な処理を停止させる
                 this.running = false
-            },
-            sendToBackendApp (target, queue) {
-                axios.post('/api/demo/' + target, queue).then(response => {
-                    console.log('send to: ' + target + ', device: ' + queue.device + ', response: ')
-                    console.log(response)
-                    this.$store.dispatch('pushToBackendQueues', {target: target, queue: queue})
-                })
-                // 本来なら vue でやり取りする必要は無いけど、可視化するため。
             },
         },
     }
